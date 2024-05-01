9721656f28054bc0773ceb6e80c8b117b6010434
Johnplumb123 <145765797+Johnplumb123@users.noreply.github.com>
2024-05-01 18:44:58 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    // Main method, entry point of the program
+    static void Main(string[] args)
+    {
+        // instance of  Game class
+        Game game = new Game();
+
+        game.Start();
+    }
+}
+
